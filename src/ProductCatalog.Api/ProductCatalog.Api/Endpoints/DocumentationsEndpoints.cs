9ae68334bc8d8af9c0704a4a97e9839a36694c06
Michal  Boczula <38834900+MichalBoczula@users.noreply.github.com>
2025-12-28 20:08:33 +0100

    Add endpoint to return all flow documentations

--- src/ProductCatalog.Api/ProductCatalog.Api/Endpoints/DocumentationsEndpoints.cs
+++ src/ProductCatalog.Api/ProductCatalog.Api/Endpoints/DocumentationsEndpoints.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using ProductCatalog.Application.Common.FlowDescriptors.Abstract;
 using ProductCatalog.Application.Common.FlowDescriptors.Common;
 using ProductCatalog.Application.Features.Currencies.Commands.CreateCurrency;
@@ -23,6 +24,51 @@ namespace ProductCatalog.Api.Endpoints
         public static IEndpointRouteBuilder MapDocumentationsEndpoints(this IEndpointRouteBuilder app)
         {
             var group = app.MapGroup("/documentation").WithTags("Documentation");
+
+            group.MapGet("/", (
+                [FromServices] IFlowDescriber<CreateProductCommand> createProductFlowDescriber,
+                [FromServices] IFlowDescriber<UpdateProductCommand> updateProductFlowDescriber,
+                [FromServices] IFlowDescriber<RemoveProductCommand> removeProductFlowDescriber,
+                [FromServices] IFlowDescriber<CreateCurrencyCommand> createCurrencyFlowDescriber,
+                [FromServices] IFlowDescriber<UpdateCurrencyCommand> updateCurrencyFlowDescriber,
+                [FromServices] IFlowDescriber<DeleteCurrencyCommand> deleteCurrencyFlowDescriber,
+                [FromServices] IFlowDescriber<CreateCategoryCommand> createCategoryFlowDescriber,
+                [FromServices] IFlowDescriber<UpdateCategoryCommand> updateCategoryFlowDescriber,
+                [FromServices] IFlowDescriber<DeleteCategoryCommand> deleteCategoryFlowDescriber,
+                [FromServices] IFlowDescriber<GetCurrenciesQuery> getCurrenciesFlowDescriber,
+                [FromServices] IFlowDescriber<GetCategoriesQuery> getCategoriesFlowDescriber,
+                [FromServices] IFlowDescriber<GetCategoryByIdQuery> getCategoryByIdFlowDescriber,
+                [FromServices] IFlowDescriber<GetProductByIdQuery> getProductByIdFlowDescriber,
+                [FromServices] IFlowDescriber<GetProductsByCategoryIdQuery> getProductsByCategoryIdFlowDescriber) =>
+            {
+                var descriptions = new List<FlowDescription>
+                {
+                    createProductFlowDescriber.DescribeFlow(default!),
+                    updateProductFlowDescriber.DescribeFlow(default!),
+                    removeProductFlowDescriber.DescribeFlow(default!),
+                    createCurrencyFlowDescriber.DescribeFlow(default!),
+                    updateCurrencyFlowDescriber.DescribeFlow(default!),
+                    deleteCurrencyFlowDescriber.DescribeFlow(default!),
+                    createCategoryFlowDescriber.DescribeFlow(default!),
+                    updateCategoryFlowDescriber.DescribeFlow(default!),
+                    deleteCategoryFlowDescriber.DescribeFlow(default!),
+                    getCurrenciesFlowDescriber.DescribeFlow(default!),
+                    getCategoriesFlowDescriber.DescribeFlow(default!),
+                    getCategoryByIdFlowDescriber.DescribeFlow(default!),
+                    getProductByIdFlowDescriber.DescribeFlow(default!),
+                    getProductsByCategoryIdFlowDescriber.DescribeFlow(default!)
+                };
+
+                return Results.Ok(descriptions);
+            })
+            .WithName("DescribeAllFlows")
+            .Produces<IReadOnlyCollection<FlowDescription>>(StatusCodes.Status200OK)
+            .Produces<ProblemDetails>(StatusCodes.Status500InternalServerError)
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Describe all request flows",
+                Description = "Returns the ordered steps executed when handling every documented request."
+            });
 
             group.MapGet("/products/create", ([FromServices] IFlowDescriber<CreateProductCommand> flowDescriber) =>
             {
