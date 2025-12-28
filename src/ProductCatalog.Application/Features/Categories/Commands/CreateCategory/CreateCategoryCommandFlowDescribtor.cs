81d640df81408755296dcd7ab0fb38e5c81b74b2
Michal  Boczula <38834900+MichalBoczula@users.noreply.github.com>
2025-12-28 18:53:39 +0100

    Add flow descriptors for category commands and documentation

---
+++ src/ProductCatalog.Application/Features/Categories/Commands/CreateCategory/CreateCategoryCommandFlowDescribtor.cs
@@ -0,0 +1,74 @@
+﻿using Mapster;
+using ProductCatalog.Application.Common.Dtos.Categories;
+using ProductCatalog.Application.Common.FlowDescriptors.Abstract;
+using ProductCatalog.Application.Common.FlowDescriptors.Common;
+using ProductCatalog.Domain.AggregatesModel.CategoryAggregate;
+using ProductCatalog.Domain.AggregatesModel.CategoryAggregate.History;
+using ProductCatalog.Domain.AggregatesModel.CategoryAggregate.Repositories;
+using ProductCatalog.Domain.Common.Enums;
+using ProductCatalog.Domain.Validation.Abstract;
+using ProductCatalog.Domain.Validation.Common;
+
+namespace ProductCatalog.Application.Features.Categories.Commands.CreateCategory
+{
+    internal sealed class CreateCategoryCommandFlowDescribtor : FlowDescriberBase<CreateCategoryCommand>
+    {
+        [FlowStep(1)]
+        public Category MapRequestToCategoryAggregate(CreateCategoryCommand command)
+        {
+            var category = command.category.Adapt<Category>();
+            return category;
+        }
+
+        [FlowStep(2)]
+        public async Task<ValidationResult> ValidateCategoryAggregate(Category category, IValidationPolicy<Category> validationPolicy)
+        {
+            var validationResult = await validationPolicy.Validate(category);
+            return validationResult;
+        }
+
+        [FlowStep(3)]
+        public void ThrowValidationExceptionIfNotValid(ValidationResult validationResult)
+        {
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult);
+            }
+        }
+
+        [FlowStep(4)]
+        public void AddCategoryToRepository(Category category, ICategoriesCommandsRepository categoriesCommandsRepository)
+        {
+            categoriesCommandsRepository.Add(category);
+        }
+
+        [FlowStep(5)]
+        public CategoriesHistory CreateCategoryHistoryEntry(Category category)
+        {
+            var categoriesHistory = category.BuildAdapter()
+               .AddParameters("operation", Operation.Inserted)
+               .AdaptToType<CategoriesHistory>();
+
+            return categoriesHistory;
+        }
+
+        [FlowStep(6)]
+        public void WriteHistoryToRepository(ICategoriesCommandsRepository categoriesCommandsRepository, CategoriesHistory categoriesHistory)
+        {
+            categoriesCommandsRepository.WriteHistory(categoriesHistory);
+        }
+
+        [FlowStep(7)]
+        public async Task SaveChanges(ICategoriesCommandsRepository categoriesCommandsRepository, CancellationToken cancellationToken)
+        {
+            await categoriesCommandsRepository.SaveChanges(cancellationToken);
+        }
+
+        [FlowStep(8)]
+        public CategoryDto MapCategoryToCategoryDto(Category category)
+        {
+            var categoryDto = category.Adapt<CategoryDto>();
+            return categoryDto;
+        }
+    }
+}
---
+++ src/ProductCatalog.Application/Features/Categories/Commands/UpdateCategory/UpdateCategoryCommandFlowDescribtor.cs
@@ -0,0 +1,102 @@
+﻿using Mapster;
+using ProductCatalog.Application.Common.Dtos.Categories;
+using ProductCatalog.Application.Common.FlowDescriptors.Abstract;
+using ProductCatalog.Application.Common.FlowDescriptors.Common;
+using ProductCatalog.Domain.AggregatesModel.CategoryAggregate;
+using ProductCatalog.Domain.AggregatesModel.CategoryAggregate.History;
+using ProductCatalog.Domain.AggregatesModel.CategoryAggregate.Repositories;
+using ProductCatalog.Domain.Common.Enums;
+using ProductCatalog.Domain.Validation.Abstract;
+using ProductCatalog.Domain.Validation.Common;
+
+namespace ProductCatalog.Application.Features.Categories.Commands.UpdateCategory
+{
+    internal sealed class UpdateCategoryCommandFlowDescribtor : FlowDescriberBase<UpdateCategoryCommand>
+    {
+        [FlowStep(1)]
+        public Category MapRequestToCategoryAggregate(UpdateCategoryCommand command)
+        {
+            var incoming = command.category.Adapt<Category>();
+            return incoming;
+        }
+
+        [FlowStep(2)]
+        public async Task<ValidationResult> ValidateIncomingCategory(Category category, IValidationPolicy<Category> validationPolicy)
+        {
+            var validationResult = await validationPolicy.Validate(category);
+            return validationResult;
+        }
+
+        [FlowStep(3)]
+        public void ThrowValidationExceptionIfIncomingInvalid(ValidationResult validationResult)
+        {
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult);
+            }
+        }
+
+        [FlowStep(4)]
+        public async Task<Category> LoadExistingCategory(Guid categoryId, ICategoriesCommandsRepository categoriesCommandsRepository, CancellationToken cancellationToken)
+        {
+            return await categoriesCommandsRepository.GetCategoryById(categoryId, cancellationToken);
+        }
+
+        [FlowStep(5)]
+        public async Task<ValidationResult> ValidateExistingCategory(Category category, IValidationPolicy<Category> validationPolicy)
+        {
+            var validationResult = await validationPolicy.Validate(category);
+            return validationResult;
+        }
+
+        [FlowStep(6)]
+        public void ThrowValidationExceptionIfExistingInvalid(ValidationResult validationResult)
+        {
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult);
+            }
+        }
+
+        [FlowStep(7)]
+        public void AssignNewCategoryInformation(Category existingCategory, Category incomingCategory)
+        {
+            existingCategory.AssigneNewCategoryInformation(incomingCategory);
+        }
+
+        [FlowStep(8)]
+        public void UpdateCategoryInRepository(Category category, ICategoriesCommandsRepository categoriesCommandsRepository)
+        {
+            categoriesCommandsRepository.Update(category);
+        }
+
+        [FlowStep(9)]
+        public CategoriesHistory CreateCategoryHistoryEntry(Category category)
+        {
+            var categoriesHistory = category.BuildAdapter()
+               .AddParameters("operation", Operation.Updated)
+               .AdaptToType<CategoriesHistory>();
+
+            return categoriesHistory;
+        }
+
+        [FlowStep(10)]
+        public void WriteHistoryToRepository(ICategoriesCommandsRepository categoriesCommandsRepository, CategoriesHistory categoriesHistory)
+        {
+            categoriesCommandsRepository.WriteHistory(categoriesHistory);
+        }
+
+        [FlowStep(11)]
+        public async Task SaveChanges(ICategoriesCommandsRepository categoriesCommandsRepository, CancellationToken cancellationToken)
+        {
+            await categoriesCommandsRepository.SaveChanges(cancellationToken);
+        }
+
+        [FlowStep(12)]
+        public CategoryDto MapCategoryToCategoryDto(Category category)
+        {
+            var categoryDto = category.Adapt<CategoryDto>();
+            return categoryDto;
+        }
+    }
+}
