46406d06940176ab8daebc246acc4e5265676fe0
Michal Boczula <michal.boczula0101@gmail.com>
2026-01-03 17:21:53 +0100

    Added CORS

--- src/ProductCatalog.Api/ProductCatalog.Api/Program.cs
+++ src/ProductCatalog.Api/ProductCatalog.Api/Program.cs
@@ -41,7 +41,22 @@ namespace ProductCatalog.Api
 
             builder.Services.AddOpenApiDocument();
 
+
+            var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>() ?? [];
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.WithOrigins(allowedOrigins)
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
             var app = builder.Build();
+
+            app.UseCors("CorsPolicy");
 
             app.UseExceptionHandler(_ => { });
 
