70c7df5cbc115ec16befe484bb2f9bd4a82dc0f7
Michal Boczula <michal.boczula0101@gmail.com>
2026-01-04 23:02:00 +0100

    Added neede method to repo

--- src/ProductCatalog.Domain/AggregatesModel/MobilePhoneAggregate/Repositories/IMobilePhonesQueriesRepository.cs
+++ src/ProductCatalog.Domain/AggregatesModel/MobilePhoneAggregate/Repositories/IMobilePhonesQueriesRepository.cs
@@ -6,7 +6,9 @@ namespace ProductCatalog.Domain.AggregatesModel.MobilePhoneAggregate.Repositorie
     public interface IMobilePhonesQueriesRepository
     {
         Task<MobilePhoneReadModel?> GetById(Guid id, CancellationToken ct);
+        Task<MobilePhoneReadModel?> GetPhones(int amount, CancellationToken ct);
         Task<IReadOnlyList<MobilePhoneReadModel>> GetByFilter(CancellationToken ct);
         Task<IReadOnlyList<MobilePhonesHistory>> GetHistoryOfChanges(CancellationToken ct);
+        Task<MobilePhonesHistory>> GetHistoryOfChanges(CancellationToken ct);
     }
 }
