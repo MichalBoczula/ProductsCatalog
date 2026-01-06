38fbaaac09f654f2f89b91de17b76db64bc9e82d
Michal  Boczula <38834900+MichalBoczula@users.noreply.github.com>
2026-01-06 23:32:17 +0100

    Add required sensor flags for mobile phones

--- src/ProductCatalog.Application/Features/MobilePhones/Commands/UpdateMobilePhone/UpdateSensorsExternalDto.cs
+++ src/ProductCatalog.Application/Features/MobilePhones/Commands/UpdateMobilePhone/UpdateSensorsExternalDto.cs
@@ -1,11 +1,13 @@
-﻿namespace ProductCatalog.Application.Features.MobilePhones.Commands.UpdateMobilePhone
+﻿using System.Text.Json.Serialization;
+
+namespace ProductCatalog.Application.Features.MobilePhones.Commands.UpdateMobilePhone
 {
     public sealed record UpdateSensorsExternalDto(
-        bool Accelerometer,
-        bool Gyroscope,
-        bool Proximity,
-        bool Compass,
-        bool Barometer,
-        bool Halla,
-        bool AmbientLight);
+        [property: JsonRequired] bool Accelerometer,
+        [property: JsonRequired] bool Gyroscope,
+        [property: JsonRequired] bool Proximity,
+        [property: JsonRequired] bool Compass,
+        [property: JsonRequired] bool Barometer,
+        [property: JsonRequired] bool Halla,
+        [property: JsonRequired] bool AmbientLight);
 }
