d82bc370e6044f98b11beed2c1d731f1fcf322ab
Michal  Boczula <38834900+MichalBoczula@users.noreply.github.com>
2026-01-24 06:26:49 +0100

    Added validation for mobile phone description2 and 3

---
+++ src/ProductCatalog.Domain/Validation/Concrete/Rules/MobilePhones/MobilePhonesDescriptionsValidationRule.cs
@@ -0,0 +1,44 @@
+﻿using ProductCatalog.Domain.AggregatesModel.MobilePhoneAggregate;
+using ProductCatalog.Domain.Validation.Abstract;
+using ProductCatalog.Domain.Validation.Common;
+
+namespace ProductCatalog.Domain.Validation.Concrete.Rules.MobilePhones
+{
+    public sealed class MobilePhonesDescriptionsValidationRule : IValidationRule<MobilePhone>
+    {
+        private readonly ValidationError description2IsNullOrWhitespace;
+        private readonly ValidationError description3IsNullOrWhitespace;
+
+        public MobilePhonesDescriptionsValidationRule()
+        {
+            description2IsNullOrWhitespace = new ValidationError
+            {
+                Message = "Description2 cannot be null or whitespace.",
+                Name = nameof(MobilePhonesDescriptionsValidationRule),
+                Entity = nameof(MobilePhone)
+            };
+            description3IsNullOrWhitespace = new ValidationError
+            {
+                Message = "Description3 cannot be null or whitespace.",
+                Name = nameof(MobilePhonesDescriptionsValidationRule),
+                Entity = nameof(MobilePhone)
+            };
+        }
+
+        public async Task IsValid(MobilePhone entity, ValidationResult validationResults)
+        {
+            if (entity == null) return;
+
+            if (string.IsNullOrWhiteSpace(entity.Description2))
+                validationResults.AddValidationError(description2IsNullOrWhitespace);
+
+            if (string.IsNullOrWhiteSpace(entity.Description3))
+                validationResults.AddValidationError(description3IsNullOrWhitespace);
+        }
+
+        public List<ValidationError> Describe()
+        {
+            return [description2IsNullOrWhitespace, description3IsNullOrWhitespace];
+        }
+    }
+}
