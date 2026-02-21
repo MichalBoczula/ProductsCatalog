f1fcf5da85f93b5bc13d72f450e71e0d07aa3a4a
Michal Boczula <michal.boczula0101@gmail.com>
2026-02-21 22:20:25 +0100

    Removed duplicated class

--- src/ProductCatalog.Application/Common/Dtos/MobilePhones/MobilePhoneFilterDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductCatalog.Application.Common.Dtos.MobilePhones
-{
-    public sealed record MobilePhoneFilterDto
-    {
-        public string Brand { get; set; }
-        public decimal MinimalPrice { get; set; }
-        public decimal MaximalPrice { get; set; }
-    }
-}
