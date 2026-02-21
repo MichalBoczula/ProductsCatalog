8b17d0586f937cb372e7ee6dd69ef5f501b9103f
Michal  Boczula <38834900+MichalBoczula@users.noreply.github.com>
2026-02-21 19:12:17 +0100

    Added amount validation policy for mobile phones query

--- src/ProductCatalog.Application/Features/MobilePhones/Queries/GetMobilePhones/GetMobilePhonesQueryFlowDescribtor.cs
+++ src/ProductCatalog.Application/Features/MobilePhones/Queries/GetMobilePhones/GetMobilePhonesQueryFlowDescribtor.cs
@@ -4,6 +4,7 @@ using ProductCatalog.Application.Common.FlowDescriptors.Abstract;
 using ProductCatalog.Application.Common.FlowDescriptors.Common;
 using ProductCatalog.Domain.AggregatesModel.MobilePhoneAggregate.ReadModel;
 using ProductCatalog.Domain.AggregatesModel.MobilePhoneAggregate.Repositories;
+using ProductCatalog.Domain.Validation.Abstract;
 using ProductCatalog.Domain.Validation.Common;
 
 namespace ProductCatalog.Application.Features.MobilePhones.Queries.GetMobilePhones
@@ -11,12 +12,27 @@ namespace ProductCatalog.Application.Features.MobilePhones.Queries.GetMobilePhon
     internal sealed class GetMobilePhonesQueryFlowDescribtor : FlowDescriberBase<GetMobilePhonesQuery>
     {
         [FlowStep(1)]
+        public Task<ValidationResult> ValidateAmount(int amount, IValidationPolicy<int> validationPolicy)
+        {
+            return validationPolicy.Validate(amount);
+        }
+
+        [FlowStep(2)]
+        public void ThrowValidationExceptionIfAmountInvalid(ValidationResult validationResult)
+        {
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult);
+            }
+        }
+
+        [FlowStep(3)]
         public Task<IReadOnlyList<MobilePhoneReadModel>> GetMobilePhones(IMobilePhonesQueriesRepository mobilePhonesQueriesRepository, int amount, CancellationToken cancellationToken)
         {
             return mobilePhonesQueriesRepository.GetPhones(amount, cancellationToken);
         }
 
-        [FlowStep(2)]
+        [FlowStep(4)]
         public IReadOnlyList<MobilePhoneReadModel> EnsureMobilePhonesFound(IReadOnlyList<MobilePhoneReadModel> mobilePhones)
         {
             if (mobilePhones is null)
@@ -27,7 +43,7 @@ namespace ProductCatalog.Application.Features.MobilePhones.Queries.GetMobilePhon
             return mobilePhones;
         }
 
-        [FlowStep(3)]
+        [FlowStep(5)]
         public IReadOnlyList<MobilePhoneDto> MapMobilePhonesToDto(IReadOnlyList<MobilePhoneReadModel> mobilePhones)
         {
             return mobilePhones.Adapt<List<MobilePhoneDto>>().AsReadOnly();
---
+++ src/ProductCatalog.Domain/Validation/Concrete/Policies/AmountValidationPolicy.cs
@@ -0,0 +1,40 @@
+﻿using ProductCatalog.Domain.Validation.Abstract;
+using ProductCatalog.Domain.Validation.Common;
+using ProductCatalog.Domain.Validation.Concrete.Rules.Common;
+
+namespace ProductCatalog.Domain.Validation.Concrete.Policies
+{
+    public sealed class AmountValidationPolicy : IValidationPolicy<int>, IValidationPolicyDescriptorProvider
+    {
+        private readonly List<IValidationRule<int>> _rules = [];
+
+        public AmountValidationPolicy()
+        {
+            _rules.Add(new AmountGreaterThanZeroValidationRule());
+        }
+
+        public Task<ValidationResult> Validate(int amount)
+        {
+            ValidationResult validationResult = new();
+            _rules.ForEach(rule => rule.IsValid(amount, validationResult));
+            return Task.FromResult(validationResult);
+        }
+
+        public ValidationPolicyDescriptor Describe()
+        {
+            var allErrors = _rules
+                .Select(rule => new ValidationRuleDescriptor()
+                {
+                    RuleName = rule.GetType().Name,
+                    Rules = rule.Describe()
+                })
+                .ToList();
+
+            return new ValidationPolicyDescriptor()
+            {
+                PolicyName = nameof(AmountValidationPolicy),
+                Rules = allErrors
+            };
+        }
+    }
+}
