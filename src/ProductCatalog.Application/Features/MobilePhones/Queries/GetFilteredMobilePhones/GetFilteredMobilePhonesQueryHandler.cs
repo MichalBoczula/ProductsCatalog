f573ff9ae388d0b48ed3239a0968a1a591deea09
Michal  Boczula <38834900+MichalBoczula@users.noreply.github.com>
2026-02-22 15:42:25 +0100

    Added validation policy for mobile phone filter prices

--- src/ProductCatalog.Application/Features/MobilePhones/Queries/GetFilteredMobilePhones/GetFilteredMobilePhonesQueryHandler.cs
+++ src/ProductCatalog.Application/Features/MobilePhones/Queries/GetFilteredMobilePhones/GetFilteredMobilePhonesQueryHandler.cs
@@ -1,16 +1,24 @@
 ﻿using MediatR;
 using ProductCatalog.Application.Common.Dtos.MobilePhones;
 using ProductCatalog.Domain.AggregatesModel.MobilePhoneAggregate.Repositories;
+using ProductCatalog.Domain.Common.Filters;
+using ProductCatalog.Domain.Validation.Abstract;
 
 namespace ProductCatalog.Application.Features.MobilePhones.Queries.GetFilteredMobilePhones
 {
     internal sealed class GetFilteredMobilePhonesQueryHandler(
         IMobilePhonesQueriesRepository _mobilePhonesQueriesRepository,
+        IValidationPolicy<MobilePhoneFilterDto> _mobilePhoneFilterValidationPolicy,
         GetFilteredMobilePhonesQueryFlowDescribtor _getFilteredMobilePhonesQueryFlowDescribtor
         ) : IRequestHandler<GetFilteredMobilePhonesQuery, IList<MobilePhoneDto>>
     {
         public async Task<IList<MobilePhoneDto>> Handle(GetFilteredMobilePhonesQuery request, CancellationToken cancellationToken)
         {
+            var validationResult = await _getFilteredMobilePhonesQueryFlowDescribtor
+                .ValidateFilter(request.filter, _mobilePhoneFilterValidationPolicy);
+
+            _getFilteredMobilePhonesQueryFlowDescribtor.ThrowValidationExceptionIfFilterInvalid(validationResult);
+
             var phones = await _getFilteredMobilePhonesQueryFlowDescribtor
                 .GetFilteredMobilePhones(_mobilePhonesQueriesRepository, request.filter, cancellationToken);
 
---
+++ src/ProductCatalog.Domain/Validation/Concrete/Policies/MobilePhoneFilterValidationPolicy.cs
@@ -0,0 +1,46 @@
+﻿using ProductCatalog.Domain.Common.Filters;
+using ProductCatalog.Domain.Validation.Abstract;
+using ProductCatalog.Domain.Validation.Common;
+using ProductCatalog.Domain.Validation.Concrete.Rules.MobilePhones;
+
+namespace ProductCatalog.Domain.Validation.Concrete.Policies
+{
+    public sealed class MobilePhoneFilterValidationPolicy : IValidationPolicy<MobilePhoneFilterDto>, IValidationPolicyDescriptorProvider
+    {
+        private readonly List<IValidationRule<MobilePhoneFilterDto>> _rules = [];
+
+        public MobilePhoneFilterValidationPolicy()
+        {
+            _rules.Add(new MobilePhoneFilterPricesValidationRule());
+        }
+
+        public async Task<ValidationResult> Validate(MobilePhoneFilterDto mobilePhoneFilter)
+        {
+            ValidationResult validationResult = new();
+
+            foreach (var rule in _rules)
+            {
+                await rule.IsValid(mobilePhoneFilter, validationResult);
+            }
+
+            return validationResult;
+        }
+
+        public ValidationPolicyDescriptor Describe()
+        {
+            var allErrors = _rules
+                .Select(rule => new ValidationRuleDescriptor()
+                {
+                    RuleName = rule.GetType().Name,
+                    Rules = rule.Describe()
+                })
+                .ToList();
+
+            return new ValidationPolicyDescriptor()
+            {
+                PolicyName = nameof(MobilePhoneFilterValidationPolicy),
+                Rules = allErrors
+            };
+        }
+    }
+}
