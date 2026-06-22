975982b437e7d69fab430de4291cc88506989efe
Michal Boczula <michal.boczula0101@gmail.com>
2026-06-22 23:38:17 +0200

    Set up for benchmark test project

---
+++ tests/ProductsCatalog.Performance.BenchmarkTests/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductsCatalog.Performance.BenchmarkTests
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //BenchmarkRunner.Run<TestHere>();
+        }
+    }
+}
+
