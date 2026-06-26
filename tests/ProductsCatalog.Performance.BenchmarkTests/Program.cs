68dd84a66dab1934c7205f65de07978783d0b843
Michal  Boczula <38834900+MichalBoczula@users.noreply.github.com>
2026-06-26 22:55:05 +0200

    Add currency validation policy benchmarks

--- tests/ProductsCatalog.Performance.BenchmarkTests/Program.cs
+++ tests/ProductsCatalog.Performance.BenchmarkTests/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BenchmarkDotNet.Running;
+using ProductCatalog.Performance.BenchmarkTests.Currencies.Domain;
 using ProductCatalog.Performance.BenchmarkTests.MobilePhones.Domain;
 
 namespace ProductsCatalog.Performance.BenchmarkTests
@@ -11,6 +12,11 @@ namespace ProductsCatalog.Performance.BenchmarkTests
             // 1. Mobile Phones / Products Domain
             // ==========================================
             BenchmarkRunner.Run<MobilePhonesValidationPolicyBenchmarks>();
+
+            // ==========================================
+            // 2. Currencies Domain
+            // ==========================================
+            BenchmarkRunner.Run<CurrenciesValidationPolicyBenchmarks>();
         }
     }
 }
