40f965223bd3b2d5d8c2241741b58ad10f07060a
Michal Boczula <michal.boczula0101@gmail.com>
2026-08-08 17:49:36 +0200

    Changed enum for string

--- src/ProductCatalog.Domain/Common/Enums/MobilePhonesBrand.cs
+++ src/ProductCatalog.Domain/Common/Enums/MobilePhonesBrand.cs
@@ -1,5 +1,8 @@
-﻿namespace ProductCatalog.Domain.Common.Enums
+﻿using System.Text.Json.Serialization;
+
+namespace ProductCatalog.Domain.Common.Enums
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum MobilePhonesBrand
     {
         Apple,
